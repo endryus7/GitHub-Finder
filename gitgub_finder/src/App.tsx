74190a699446f3eee12c652d5a92bc4aa7b2dcf8
Endryus-Montezano <endryusmontezano@live.com>
2026-03-02 18:43:09 -0300

    Configura o router

--- gitgub_finder/src/App.tsx
+++ gitgub_finder/src/App.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom"
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>GitHub Finder</h1>
+      <Outlet />
     </div>
   )
 }
