26307cc508ab466199d8ee3e1e941ffb9ee6e355
Endryus-Montezano <endryusmontezano@live.com>
2026-03-03 16:56:53 -0300

    Cria componente de erro

--- gitgub_finder/src/routes/Home.tsx
+++ gitgub_finder/src/routes/Home.tsx
@@ -1,17 +1,28 @@
 import type { UserProps } from "../types/user"
 
+// Componentes
 import Search from "../components/Search"
 import User from "../components/User"
+import Erro from "../components/Erro"
 
 import { useState } from "react"
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null)
+  const [error, setError] = useState(false)
 
   const loadUser = async(userName: string) => {
+    setError(false)
+    setUser(null)
+
     const res = await fetch(`https://api.github.com/users/${userName}`)
 
     const data = await res.json()
+
+    if(res.status === 404) {
+      setError(true)
+      return
+    }
 
     const {avatar_url, login, location, followers, following} = data
 
@@ -30,6 +41,7 @@ const Home = () => {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && <Erro />}
     </div>
   )
 }
